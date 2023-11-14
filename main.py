f6850fedfca2c39983bf88853081871b49b6a2c4
biphan380 <biphan380@gmail.com>
2023-11-13 19:16:27 -0500

    build doc summary index

--- main.py
+++ main.py
@@ -52,10 +52,52 @@ for title in wiki_titles:
 # Load all wiki documents
 # Each txt file is its own Document object?
 city_docs = []
+file_counter = 1
 for wiki_title in wiki_titles:
     docs = SimpleDirectoryReader(
         input_files=[f"data/{wiki_title}.txt"]
     ).load_data()
     docs[0].doc_id = wiki_title
     city_docs.extend(docs)
+    
+    with open(f'doc_objects/city_docs_{file_counter}.txt', 'w') as f:
+        for doc in city_docs:
+            f.write(f'{doc.doc_id}\n')
+            f.write(f'{doc.text}\n\n')
+            f.write('======================== new doc object starts ================================\n')
+    file_counter += 1
+
 print(len(city_docs))
+
+sys.exit()
+
+
+
+
+
+
+# Build Document Summary Index
+chatgpt = OpenAI(temperature=0, model="gpt-3.5-turbo")
+service_context = ServiceContext.from_defaults(llm=chatgpt, chunk_size=1024)
+
+# default mode of building the index
+response_synthesizer = get_response_synthesizer(
+    response_mode="tree_summarize", use_async=True
+)
+doc_summary_index = DocumentSummaryIndex.from_documents(
+    city_docs,
+    service_context=service_context,
+    response_synthesizer=response_synthesizer,
+    show_progress=True,
+)
+
+doc_summary_index.get_document_summary("Boston")
+
+doc_summary_index.storage_context.persist("index")
+
+from llama_index.indices.loading import load_index_from_storage
+from llama_index import StorageContext
+
+# rebuild storage context
+storage_context = StorageContext.from_defaults(persist_dir="index")
+doc_summary_index = load_index_from_storage(storage_context)
