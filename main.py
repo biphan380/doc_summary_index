8cc48a44129e262cefedd320a1f5437a69389bc5
biphan380 <biphan380@gmail.com>
2023-11-15 15:32:25 -0500

    inject llamaCPP

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ for title in wiki_titles:
             # 'exintro': True,
             "explaintext": True,
         },
+        timeout=30
     ).json()
     page = next(iter(response["query"]["pages"].values()))
     wiki_text = page["extract"]
@@ -69,31 +70,44 @@ for wiki_title in wiki_titles:
 
 print(len(city_docs))
 
-sys.exit()
+# sys.exit()
 
+from llama_index.llms import LlamaCPP
+from llama_index.llms.llama_utils import messages_to_prompt, completion_to_prompt
 
-
-
-
+llamaCPP = LlamaCPP(
+    model_url = None,
+    model_path="models/llama-2-13b-chat.Q4_0.gguf",
+    temperature=0.1,
+    max_new_tokens=256,
+    context_window=3900,
+    generate_kwargs={},
+    model_kwargs={"n_gpu_layers": 1},
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=True,
+)
 
 # Build Document Summary Index
-chatgpt = OpenAI(temperature=0, model="gpt-3.5-turbo")
-service_context = ServiceContext.from_defaults(llm=chatgpt, chunk_size=1024)
+# chatgpt = OpenAI(temperature=0, model="gpt-3.5-turbo")
+
+# Need a local embed model
+service_context = ServiceContext.from_defaults(llm=llamaCPP, chunk_size=1024)
 
 # default mode of building the index
 response_synthesizer = get_response_synthesizer(
     response_mode="tree_summarize", use_async=True
 )
-doc_summary_index = DocumentSummaryIndex.from_documents(
-    city_docs,
-    service_context=service_context,
-    response_synthesizer=response_synthesizer,
-    show_progress=True,
-)
+# doc_summary_index = DocumentSummaryIndex.from_documents(
+#     city_docs,
+#     service_context=service_context,
+#     response_synthesizer=response_synthesizer,
+#     show_progress=True,
+# )
 
-doc_summary_index.get_document_summary("Boston")
+# doc_summary_index.get_document_summary("Boston")
 
-doc_summary_index.storage_context.persist("index")
+# doc_summary_index.storage_context.persist("index")
 
 from llama_index.indices.loading import load_index_from_storage
 from llama_index import StorageContext
